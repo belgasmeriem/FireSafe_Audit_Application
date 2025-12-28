6a5751cca266916a8e4ef643fdc32fa2827b6b3e
belgasmeriem <belgasmeriem@gmail.com>
2025-12-28 15:07:18 +0100

    Initial commit - FireSafe Audit Spring Boot project

---
+++ src/test/java/org/xproce/firesafe_audit/FireSafeAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xproce.firesafe_audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireSafeAuditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
